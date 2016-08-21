1d8858f6ed6b54995eeea9d4ce48a7a6d805d48b
marty-hope <marty.hope@gmail.com>
2016-08-21 16:02:30 -0700

    Add more formatting

--- main.elm
+++ main.elm
@@ -1,6 +1,6 @@
 import Html exposing (text)
 
-fib : number -> number
+fib : Int -> Int
 fib n =
     case n of
     0 -> 0
@@ -15,10 +15,22 @@ getEven : List Int -> List Int
 getEven numbers =
     List.filter isEven numbers
 
+list: List number
+list = [1,2,3,4,5,6,7,8,9]
 
+fibIndex: Int
+fibIndex = 15
 
 main : Html.Html a
 main =
-    text (toString (fib 34) ++ " " ++
-    --toString (getEven [1,2,3,4,5,6,7,8,9]))
-    (toString (fib 3)))
+    text ("Get Even Numbers in"
+    ++ " "
+    ++ (toString list)
+    ++ " => "
+    ++ (toString (getEven list))
+    ++ " "
+    ++ "Determine fibonnaci value at index"
+    ++ " "
+    ++ (toString fibIndex)
+    ++ " => "
+    ++ (toString (fib fibIndex)))
