439dcf69333b8faad11e4c8bd7bfe179bed41dd9
marty-hope <marty.hope@gmail.com>
2016-08-21 15:36:25 -0700

    Test fib implementation

--- main.elm
+++ main.elm
@@ -20,4 +20,5 @@ getEven numbers =
 main : Html.Html a
 main =
     text (toString (fib 34) ++ " " ++
-    toString (getEven [1,2,3,4,5,6,7,8,9]))
+    --toString (getEven [1,2,3,4,5,6,7,8,9]))
+    (toString (fib 3)))
