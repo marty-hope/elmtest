ed69bb5c1e7745c36e9aa278c140c57c6a2baae2
marty-hope <marty.hope@gmail.com>
2016-08-21 20:29:51 -0700

    Beautify html

--- main.elm
+++ main.elm
@@ -1,4 +1,4 @@
-import Html exposing (text)
+import Html exposing (text,div,p)
 
 fib : Int -> Int
 fib n =
@@ -15,22 +15,38 @@ getEven : List Int -> List Int
 getEven numbers =
     List.filter isEven numbers
 
+isOdd : Int -> Bool
+isOdd n =
+     n % 2 /= 0
+
+getOdd : List Int -> List Int
+getOdd numbers =
+    List.filter isOdd numbers
+
 list: List number
-list = [1,2,3,4,5,6,7,8,9]
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 fibIndex: Int
-fibIndex = 15
+fibIndex = 30
 
-main : Html.Html a
 main =
-    text ("Get Even Numbers in"
+    div []
+
+    [ p [] [ text ("Get Even Numbers in"
+            ++ " "
+            ++ (toString list)
+            ++ " => "
+            ++ (toString (getEven list)))]
+
+    ,p [] [ text ("Get Odd Numbers in"
     ++ " "
     ++ (toString list)
     ++ " => "
-    ++ (toString (getEven list))
-    ++ " "
-    ++ "Determine fibonnaci value at index"
+    ++ (toString (getOdd list)))]
+
+    ,p [] [ text ("Determine fibonnaci value at index"
     ++ " "
     ++ (toString fibIndex)
     ++ " => "
-    ++ (toString (fib fibIndex)))
+    ++ (toString (fib fibIndex)))]
+    ]
